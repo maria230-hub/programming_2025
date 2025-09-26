d99fc237149e677bae86957489231f4581ebe6b1
maria230-hub <nadya911450762043@gmail.com>
2025-09-26 23:10:42 +0200

    ADD: Hello, world, lol
    yaaas

---
+++ lecture01_basics/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int spaceofint = sizeof(int) * 8;
+    int sizeoffloat = sizeof(float) * 8;
+    std::cout << "int, занимает" << spaceofint << "бит(а), мин. значение = " << INT_MIN << ", макс. значение = " << INT_MAX << std::endl;
+    std::cout << "float, занимает" << sizeoffloat << "бит(а), макс. значение = " << std::numeric_limits<float>::max() << ", мин. значение = " << std::numeric_limits<float>::min() << std::endl;
+    unsigned int a, b; // так как катеты прям-го треугольника целые&положительные числа => тип данных unsigned int
+    std::cout << "введи число b" << std::endl;
+    std::cin >> b;
+    std::cout << "введи число a" << std::endl;
+    std::cin >> a;
+    float c = sqrt(a * a + b * b); //кв.корень может быть нецелым числом :( => тип данных float
+    int f = c + a + b;
+    std::cout << "значение с = " << c << std::endl;
+    std::cout << "значение периметра = " << f << std::endl;
+}
