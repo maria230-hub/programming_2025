d9f4f611441585d7e55b17fc8edf639498dfb833
maria230-hub <nadya911450762043@gmail.com>
2025-11-07 11:02:47 +0200

    ADD: cho
    kek

---
+++ lecture 05_swag/ConsoleApplication5.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	const int PMax = 4;
+	int x[PMax];
+	int mini[PMax];
+	cout << "Введите элементы массива = " << " ";
+	for (int g = 0; g < PMax; ++g) {
+		cin >> x[g];
+		int temp = abs(x[g]);
+		mini[g] = 9;
+		if (temp == 0) {
+			mini[g] = 0;
+		}
+		else {
+			while (temp > 0) {
+				int num = temp % 10;
+				if (num < mini[g]) {
+					mini[g] = num;
+				}
+				temp /= 10;
+			}
+		}
+	}
+	bool checkx = false;
+	bool checkx = false;
+	if (mini[0] == mini[PMax - 1]) {
+		checkx = true;
+	}
+	if (checkx) {
+		for (int i = 0; i < PMax - 1; ++i) {
+			for (int j = 0; j < PMax - i - 1; ++j) {
+				if (x[j] > x[j + 1]) {
+					swap(x[j], x[j + 1]);
+				}
+			}
+		}
+		cout << "массив = " << " ";
+	}
+	else {
+		cout << "Массив осталося неизменным: " << " ";
+	}
+	for (int g = 0; g < PMax; ++g) {
+		cout << x[g] << " ";
+	}
+	cout << endl;
+    const int NMax = 3;
+    const int MMax= 4;
+    int matrix[NMax][MMax];
+
+    cout << "Введите элементы матрицы:" << endl;
+    for (int i = 0; i < NMax; ++i) {
+        cout << "Строка " << i << " = ";
+        for (int j = 0; j < MMax; ++j) {
+            cin >> matrix[i][j];
+        }
+    }
+    int minD = INT_MAX;
+    int t = 0;
+
+    cout << "Суммы строк:" << endl;
+    for (int i = 0; i < NMax; ++i) {
+        int Sum = 0;
+        for (int j = 0; j < MMax; ++j) {
+            Sum += matrix[i][j];
+        }
+
+		int df;
+		if (Sum == INT_MIN) {
+			df = INT_MAX;
+		}
+		else {
+			df = abs(Sum);
+		}
+        cout << "Строка " << i + 1<< ": сумма = " << Sum << endl;
+
+        if (df < minD) {
+            minD = df;
+            t = i; 
+        }
+    }
+
+    cout << "Строка " << t + 1 << " имеет сумму, значение которой наиболее приближенное к нулю" << endl;
+    for (int j = 0; j < MMax; j++) {
+        matrix[t][j] = 0;
+    }
+    cout << "Новая матрица:" << endl;
+    for (int i = 0; i < NMax; ++i) {
+        for (int j = 0; j < MMax; ++j) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
---
+++ lecture 06_kek/ConsoleApplication6.cpp
@@ -0,0 +1,98 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int sumofdigits(int a, int b) {
+    cout << "исп. функция суммы 2-х чисел: " << endl;
+    return a + 2 * b + 3;
+}
+
+int sumofdigits(int a, int b, int c) {
+    cout << "исп. функция суммы 3-х чисел: " << endl;
+    int firstsum = a + b;
+    int secondsum = a + c;
+    int thirdsum = b + c;
+
+    int maxofsum = firstsum;
+    if (secondsum > maxofsum) {
+        maxofsum = secondsum;
+    }
+    if (thirdsum > maxofsum) {
+        maxofsum = thirdsum;
+    }
+    return maxofsum;
+}
+
+
+void digitinput1() {
+    cout << "исп. функция пункта 1 " << endl;
+    int x, y, z;
+    cout << "Введите числа: ";
+    cin >> x >> y >> z;
+    int result;
+    if (x == 0) {
+        result = sumofdigits(y, z);
+    }
+    else if (y == 0) {
+        result = sumofdigits(x, z);
+    }
+    else if (z == 0) {
+        result = sumofdigits(x, y);
+    }
+    else {
+        result = sumofdigits(x, y, z);
+    }
+    cout << "Результат: " << result << endl;
+}
+
+void findTriangle(int a, int b) {
+    cout << "исп. функция вычисления гипотенузы и периметра " << endl;
+
+    if (a <= 0 || b <= 0) {
+        cout << "Катеты должны быть положительными" << endl;
+        return;
+    }
+    double c = sqrt(a * a + b * b);
+    cout << "гипотенуза = " << c << endl;
+    cout << "периметр = " << a + b + c << endl;
+}
+
+void digitinput2(int p, int k) {
+    cout << "исп. функция пункта 2: " << endl;
+    findTriangle(p, k);
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int choice;
+
+    do {
+        cout << "Выбор пункта (1 или 2) = ";
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            digitinput1();
+            break;
+        case 2: {
+            int p, k;
+            cout << "Введите катет p = ";
+            cin >> p;
+            cout << "Введите катет k = ";
+            cin >> k;
+            digitinput2(p, k);
+            break;
+        }
+        default:
+            cout << "Выберите 1 или 2" << endl;
+            break;
+        }
+
+        cout << "Продолжить? (1-да, 0-нет) ";
+        cin >> choice;
+
+    } while (choice != 0);
+
+    cout << "Конец программы " << endl;
+    return 0;
+}
